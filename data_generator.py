5e7e7482edc6b8c3c8c7bf9c8ea2f06d43591777
Meet Guleria <meetguleria94@gmail.com>
2023-08-07 16:09:39 -0700

    1day-readings-generated

--- data_generator.py
+++ data_generator.py
@@ -1,7 +1,8 @@
 from kafka import KafkaProducer
 import json
-import time
+import datetime
 import random
+import time
 
 #Creating a producer instance
 producer = KafkaProducer(
@@ -12,15 +13,15 @@ producer = KafkaProducer(
 #List of sensors
 sensors = ['sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5']
 
-while True:
+def generate_reading(timestamp):
 	#Generating random readings
 	reading = {
 		'sensor_id': random.choice(sensors),
 		'location': {
-		'lat': round(random.uniform(-90, 90), 6),
+				'lat': round(random.uniform(-90, 90), 6),
                 'lon': round(random.uniform(-180, 180), 6),
 		},
-		'timestamp': time.time(),
+    	'timestamp': timestamp.isoformat(),
 		'readings': {
 			'PM2.5': random.uniform(0, 500),
 			'PM10': random.uniform(0, 500),
@@ -30,4 +31,16 @@ while True:
 
 	producer.send('air_quality', reading)
 
-	time.sleep(1)
+num_readings = 1440
+
+start_time = datetime.datetime.now() - datetime.timedelta(hours=24)
+
+interval = datetime.timedelta(hours=24) / num_readings
+
+for _ in range(num_readings):
+	generate_reading(start_time)
+	start_time += interval
+
+while True:
+	generate_reading(datetime.datetime.now())
+	time.sleep(60)
